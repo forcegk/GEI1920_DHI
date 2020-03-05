2439c404cf8eb8ab05a5b7fbc5273844183627fa
Alonso <alonso.rodriguez@udc.es>
2020-03-05 12:01:41 +0100

    p3 base

--- p3/p3.ino
+++ p3/p3.ino
@@ -1,128 +1,13 @@
-#include <MsTimer2.h>
-#include <avr/sleep.h>
-#include <avr/interrupt.h>
+#include <Wire.h>
 
 #define debounce_ms 50
 
-const int red_led = 10;
-const int green_led = 5;
-const int button = 3;
+#define default_p1 1
+#define default_p2 2
+#define default_p3 5
+#define default_p4 10
 
-typedef struct _reloj{
-  // Con 8 bits sin signo nos llegan
-  uint_fast8_t h = 0;
-  uint_fast8_t min = 0;
-  uint_fast8_t sec = 0;
-} reloj;
-
-// Se va a acceder en ISR
-volatile reloj r1;
-volatile bool power_on = true;
-volatile bool led_status = true;
-
-long hora_recibida;
-int hora_recibida_h;
-int hora_recibida_min;
-int hora_recibida_sec;
-
-void set_time(volatile reloj *r, int h, int min, int sec){
-
-  if(h>23 || min>59 || sec>59){
-    go_to_sleep();
-    return;
-  }
-
-  r->h    = (uint_fast8_t) h;
-  r->min  = (uint_fast8_t) min;
-  r->sec  = (uint_fast8_t) sec;
-}
-
-void sumar_uno(){
-  // Incrementar segundos
-  (r1.sec)++;
-
-  if(r1.sec>59){
-    r1.sec = 0;
-    // Incrementar minutos
-    (r1.min)++;
-
-    if(r1.min>59){
-      r1.min = 0;
-      // Incrementar horas
-      (r1.h)++;
-
-      if(r1.h>23){
-        // Reset de horas
-        r1.h = 0;
-      }
-    }
-  }
-
-  if(power_on){
-    if(r1.h<10){Serial.print(0);} Serial.print(r1.h);Serial.print(":");
-    if(r1.min<10){Serial.print(0);} Serial.print(r1.min);Serial.print(":");
-    if(r1.sec<10){Serial.print(0);} Serial.println(r1.sec);
-    //Serial.print("Estado: "); Serial.println(power_on);
-    if(led_status){
-      ledWrite(HIGH, LOW);
-    }else{
-      ledWrite(LOW, HIGH);
-    }
-  }
-  // Actualizamos la posición de los leds
-  led_status = !led_status;
-}
-
-void pin3_interrupt(){
-  if(!power_on){
-    sleep_disable();
-    // Desactivamos la interruptión
-    detachInterrupt(digitalPinToInterrupt(button));
-    //Serial.print("Interrupt detached! Status:"); Serial.println(power_on);
-    Serial.flush();
-    power_on = true;
-  }
-}
-
-void _sleepNow(){
-  power_on = false;
-  sleep_enable();
-  sleep_mode(); // entra en sleep
-}
-
-void go_to_sleep(){
-  sleep_enable();
-  // Activamos la interrupción para despertar
-  attachInterrupt(digitalPinToInterrupt(button), pin3_interrupt, HIGH);
-
-  /*SAFE ZONE*/
-
-    ledWrite(LOW, LOW);
-
-    //Serial.println("Interrupt attached");
-    Serial.println("Pasando a Modo Sleep...");
-    Serial.flush();
-
-  /*END SAFE ZONE*/
-
-  set_sleep_mode(SLEEP_MODE_EXT_STANDBY);
-  cli();
-  sleep_bod_disable();
-  sei();
-
-  power_on = false;
-
-  while(!power_on){
-    _sleepNow();
-  }
-
-  sleep_disable();
-  
-  // Limpiamos el buffer por si nos han mandado basura
-  while(Serial.peek() != -1){
-      Serial.read();
-    }
-}
+#define i2c_address 0x38
 
 /**************************** PIN READ & LED ****************************/
 int debounce_digitalRead(int pin){
@@ -133,45 +18,46 @@ int debounce_digitalRead(int pin){
 
   return (prev == digitalRead(pin)) ? prev : debounce_digitalRead(pin);
 }
-
-void ledWrite(int _green_led_value, int _red_led_value){
-  digitalWrite(green_led, _green_led_value);
-  digitalWrite(red_led, _red_led_value);
-}
 /************************************************************************/
 
-void setup(){
-  // PinMode Set
-  pinMode(red_led, OUTPUT);
-  pinMode(green_led, OUTPUT);
-  pinMode(button, INPUT_PULLUP);
+void i2c_write(uint8_t led_r_l){
+  Wire.beginTransmission(i2c_address);
 
+  switch(led_r_l){
+    case (uint8_t) 1:
+      Wire.write(0b1110);
+      break;
+    
+    case (uint8_t) 2:
+      Wire.write(0b1101);
+      break;
+
+    case (uint8_t) 3:
+      Wire.write(0b1011);
+      break;
+
+    case (uint8_t) 4:
+      Wire.write(0b0111);
+      break;
+  }
+
+  Wire.endTransmission();
+}
+
+void setup(){
   // Enable serial interface
   Serial.begin(115200);
-
-  MsTimer2::set(1000, sumar_uno);
-  MsTimer2::start();
+  Wire.begin();
 }
 
 void loop(){
-
-  if(Serial.available()){
-    hora_recibida = Serial.parseInt(SKIP_ALL, ':');
-
-    // Limpiamos la basura que quede después
-    while(Serial.peek() != -1){
-      Serial.read();
-    }
-
-    if(hora_recibida >= 0){
-      hora_recibida_sec = hora_recibida % 100;
-      hora_recibida_min = hora_recibida % (100*100) / 100;
-      hora_recibida_h   = hora_recibida / (100*100);
-
-      //Serial.print("Hora recibida: "); Serial.println(hora_recibida);
-      //Serial.print("Estado: "); Serial.println(power_on);
-      set_time(&r1, hora_recibida_h, hora_recibida_min, hora_recibida_sec);
-    }
-  }
+  i2c_write(1);
+  delay(1000);
+  i2c_write(2);
+  delay(1000);
+  i2c_write(3);
+  delay(1000);
+  i2c_write(4);
+  delay(1000);
 
 }
