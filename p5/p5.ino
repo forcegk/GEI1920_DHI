5f31b469e647994f765388783fd291f7d5596c82
Alonso <alonso.rodriguez@udc.es>
2020-04-16 11:11:14 +0200

    P5 CORREGIDA Y TERMINADA

--- p5/p5.ino
+++ p5/p5.ino
@@ -2,7 +2,7 @@
 
 // Valores constantes
 const float pi= 3.1416;
-const unsigned int N= 200; // Número de muestras por ciclo
+const unsigned int N= 100; // Número de muestras por ciclo
 const unsigned int Ts= 5; // Intervalo entre muestras en ms
 // Período de la señal T= N*Ts (ms)
 
@@ -13,81 +13,51 @@ const unsigned int total_sig = 6; // Total de señales
 volatile unsigned int curr_t_sig = 0;
 volatile unsigned int curr_sig = 1; // Señal actual. Empezamos en 1
 volatile unsigned int n= 0; // Número de muestra (0 a N-1)
-volatile float x; // Valores calculados de función seno + DC
+volatile double x; // Valores calculados de función seno + DC
 
-const float N_4 = (float) N / 4.0;
-const float N_5 = (float) N / 5.0;
+const double N_4 = (double) N / 4.0;
+const double N_5 = (double) N / 5.0;
 
 
 void generar1(){
-  x= 2.5 + 1.25*sin(2*pi*n/N);
-  if (++n== N) { // Incrementa índice n de muestra
-    n= 0;
-  }  
-  // Envía dato muestra señal cada Ts ms
-  Serial.println(x); //  Envía muestra señal a serial plotter
+  x=(double) (2.5 + 1.25*sin(2*pi*(double)n/(double)N));
 }
 
 void generar2(){
-  x= 2.5 + 1.25*sin(20*pi*n/N);
-  if (++n== N) { // Incrementa índice n de muestra
-    n= 0;
-  }  
-  // Envía dato muestra señal cada Ts ms
-  Serial.println(x); //  Envía muestra señal a serial plotter
+  x=(double) (2.5 + 1.25*cos(20.0*pi*(double)n/(double)N));
 }
 
 void generar3(){
-  x= 2.5 + (1+0.5*sin(2*pi*n/N))*cos(20*pi*n/N);
-  if (++n== N) { // Incrementa índice n de muestra
-    n= 0;
-  }  
-  // Envía dato muestra señal cada Ts ms
-  Serial.println(x); //  Envía muestra señal a serial plotter
+  x=(double) (2.5 + (1.0+0.5*sin(2.0*pi*(double)n/(double)N))*cos(20.0*pi*(double)n/(double)N));
 }
 
 void generar4(){
   // N_4 = N/4 para menos calculos
   if(0<=n && n<N_4){
-    x = 2.5 - 1.25*(-4*(float)n/(float)N);
+    x =(double) (2.5 - 1.25*(-4.0*(double)n/(double)N));
   }else if(N_4<=n && n<(3*N_4)){
-    x = 2.5 + 1.25*(2-(4*(float)n/(float)N));
+    x =(double) (2.5 + 1.25*(2.0-(4.0*(double)n/(double)N)));
   }else{
-    x = 2.5 - 1.25*(4-(4*(float)n/(float)N));
+    x =(double) (2.5 - 1.25*(4.0-(4.0*(double)n/(double)N)));
   }
-  if (++n== N) { // Incrementa índice n de muestra
-    n= 0;
-  }  
-  // Envía dato muestra señal cada Ts ms
-  Serial.println(x); //  Envía muestra señal a serial plotter
 }
 
 void generar5(){
   // N_4 = N/4 para menos calculos
   if(n<N_5){
-    x = 5;
+    x = 5.0;
   }else{
     x = 0;
-  }
-  if (++n== N) { // Incrementa índice n de muestra
-    n= 0;
-  }  
-  // Envía dato muestra señal cada Ts ms
-  Serial.println(x); //  Envía muestra señal a serial plotter
+  } 
 }
 
 void generar6(){
   // N_4 = N/4 para menos calculos
   if(n<N_5){
-    x = 2.5 + 1.25*cos(20*pi*n/N);
+    x =(double) (2.5 + 1.25*cos(20*pi*(double)n/(double)N));
   }else{
-    x = 2.5 - 1.25*cos(20*pi*n/N);
+    x =(double) (2.5 - 1.25*cos(20*pi*(double)n/(double)N));
   }
-  if (++n== N) { // Incrementa índice n de muestra
-    n= 0;
-  }  
-  // Envía dato muestra señal cada Ts ms
-  Serial.println(x); //  Envía muestra señal a serial plotter
 }
 
 void (*signals[total_sig]) () = {generar1, generar2, generar3, generar4, generar5, generar6};
@@ -109,6 +79,9 @@ void execute_function( void (*f)(void) ){
 void generar(){
   execute_function(signals[curr_sig-1]);
 
+  // Envía dato muestra señal cada Ts ms
+  Serial.println(x); //  Envía muestra señal a serial plotter
+
   if (++n== N) { // Incrementa índice n de muestra
     n = 0;
   }
@@ -120,6 +93,4 @@ void generar(){
       curr_sig = 1;
     }
   }
-// Envía dato muestra señal cada Ts ms
-  Serial.println(x); //  Envía muestra señal a serial plotter
 }
